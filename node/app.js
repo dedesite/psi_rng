b8489900204d873ebb67ae36635debbf8e7ae9e1
Andréas Livet <andreas.livet@gmail.com>
2012-09-04 23:00:00 +0200

    Socket.io server now working with serialPort !
    -It sends all the generated numbers each 100ms and work fine with the
    processingjs apps !
    -Next step : use a Raspberry pi instead of Arduino to generate numbers
    and send them via websockets !

--- node/app.js
+++ node/app.js
@@ -1,43 +1,52 @@
 var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
-/*var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("/dev/ttyUSB0", { baudrate : 19200 });*/
+var SerialPort = require("serialport").SerialPort
+var serialPort = new SerialPort("/dev/ttyUSB0", { baudrate : 19200 });
 
 //A la place des hardware random on met du pseudo random pour commencer
-function getRandomInt(min, max)
+/*function getRandomInt(min, max)
 {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+}*/
 
+var buffer_to_send = [];
 var numbers_to_send = [];
 server.listen(8080);
+
+serialPort.on("data", function (data) {
+  //Don't fill the buffer if there is no connections
+  if(io.sockets.clients().length > 0){
+    buffer_to_send.push(data);
+  }
+});
 
 io.set("log level", 1);
 io.sockets.on('connection', function (socket) {
   //socket.emit('news', { hello: 'world' });
-  console.log("Connection established");
-  socket.emit('welcome', {message : "Hello"});
+  if(io.sockets.clients().length == 1){
+    console.log("Connection established");
+    socket.emit('welcome', {message : "Hello"});
 
-  //On simule la génération de 37 chiffres (c'est a peu près la vitesse du RNG actuelle) toutes les 100 milliseconds
-  function send_numbers(){
-    for(var i = 0 ; i < 37 ; i++){
-      numbers_to_send.push(getRandomInt(0, 255));
-    }
-    socket.emit('serial', numbers_to_send);
-    numbers_to_send = [];
-    setTimeout(send_numbers, 100);
-  }
-
-  send_numbers();
-
-
-  /*serialPort.on("data", function (data) {
-  	//console.log("data");
-  	numbers_to_send.push(data);
-  	if(numbers_to_send.length >= 100){
-  		socket.emit('serial', numbers_to_send);
+    function send_numbers(){
+      for(var i = 0 ; i < buffer_to_send.length ; i++){
+        for(var j = 0 ; j < buffer_to_send[i].length ; j++){
+          numbers_to_send.push(buffer_to_send[i].readUInt8(j));  
+        }
+      }
+      socket.emit('serial', numbers_to_send);
       numbers_to_send = [];
-  	}
-  });*/
+      buffer_to_send = [];
+      //Don't send numbers if there is no more connections
+      if(io.sockets.clients().length > 0){
+        setTimeout(send_numbers, 100);  
+      }
+    }
+
+    send_numbers();
+  }
+  else{
+    console.log("Connection refused : only one client can connect at a time");
+    socket.emit('refused', {message : "Too much connections"});
+  }
 });
