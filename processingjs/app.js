403339570f4e2d5eb3021a2cb46a67ae71a02f17
Andréas Livet <andreas.livet@gmail.com>
2013-06-14 17:03:57 +0200

    Start to save xp data and display them
    -Now it's just a very simple Redis hash save using node.js but in the
    futur it will be save in a MySql database using Ruby
    -I tried to reproduice the type of graph used by the PEAR laboratory
    (and others) in their studies. I used g.raphael js library and modify it
    a bit to draw only certain lines with dashes.
    -Rng can now send xp data

--- processingjs/app.js
+++ processingjs/app.js
@@ -1,4 +1,30 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+app.use(express.bodyParser());
+var redis = require("redis"),
+    client = redis.createClient();
+
+//Temp : when I need to delete test results
+//client.del("psi.xp.results");
+
+app.post('/results', function(req, res){
+  var name = req.body.name;
+  client.hget("psi.xp.results", name, function (err, reply) {
+    var table = reply != null ? reply.split(',') : [];
+    table.push(req.body.data);
+    client.hset("psi.xp.results", name, table);
+    res.send("ok");
+  });
+});
+
+app.get('/results.json', function(req, res) {
+  client.hgetall("psi.xp.results", function (err, reply) {
+    var body = JSON.stringify(reply)
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Length', body.length);
+    res.end(body);
+  });
+});
 
 app.get('/*', function (req, res) {
 	res.sendfile(__dirname + req.originalUrl);
